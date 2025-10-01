df74848ed252c4745efa403512910aa2ee736bc4
Varma Cephas <varmac231@gmail.com>
2025-10-01 21:34:39 +0200

    feat: implement navigation and header, add logic for logged in and non logged in users in nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,40 @@
+import Logo from '../assets/Logo'
+import ProfileIcon from '../assets/ProfileIcon'
+import clsx from 'clsx'
+import NavigationMenu from './menu-bar/NavigationMenu'
+import { useContext, useState } from 'react'
+import { contextAuth } from '../context/AuthContext'
+
 export default function Header() {
-  return <header>Menubar</header>
+  const { loggedIn } = useContext(contextAuth)
+  const [showNavgation, setShowNavigation] = useState(false)
+  const headerClasses = clsx(
+    'absolute flex w-full items-center rounded-b-xl bg-gray-800 px-4 py-3 text-white',
+    {
+      'justify-between': loggedIn,
+      'justify-center py-6': !loggedIn,
+    },
+  )
+  function handleShowNavMenu() {
+    setShowNavigation(prev => !prev)
+  }
+  return (
+    <>
+      <header className={headerClasses}>
+        {loggedIn && (
+          <button onClick={handleShowNavMenu}>{showNavgation ? 'Close' : 'Menu'}</button>
+        )}
+
+        {loggedIn && (
+          <div>
+            <ProfileIcon />
+          </div>
+        )}
+      </header>
+      <div className="absolute inset-x-0 mx-auto w-[20%] rounded-b-full bg-gray-800  py-2 pb-5">
+        <Logo className="w-[99%]" />
+      </div>
+      <NavigationMenu showNavigation={showNavgation} />
+    </>
+  )
 }
---
+++ src/components/menu-bar/NavigationMenu.tsx
@@ -0,0 +1,48 @@
+import { NavLink } from 'react-router-dom'
+import CarIcon from '../../assets/CarIcon'
+import DateIcon from '../../assets/DateIcon'
+import CarsIcon from '../../assets/CarsIcon'
+import ListIcon from '../../assets/ListIcon'
+import CarPlusIcon from '../../assets/CarPlusIcon'
+import LogoutIcon from '../../assets/LogoutIcon'
+
+interface showNavgation {
+  showNavigation: boolean
+}
+export default function NavigationMenu({ showNavigation }: showNavgation) {
+  return (
+    showNavigation && (
+      <nav className="absolute ml-4 mt-20 animate-[pulse_0.6s_ease-in-out_1] rounded-xl bg-gray-800 p-5 text-white">
+        <div className="border-b">
+          <NavLink to="" className="flex gap-4 pb-2">
+            <CarIcon />
+            Book a Car
+          </NavLink>
+          <NavLink to="" className="flex gap-4 pb-2">
+            <DateIcon />
+            My Bookings
+          </NavLink>
+        </div>
+        <h3 className="py-4 font-bold">My cars</h3>
+        <div className="border-b">
+          <NavLink to="" className="flex gap-4 pb-2">
+            <CarsIcon />
+            See My Cars
+          </NavLink>
+          <NavLink to="" className="flex gap-4 pb-2">
+            <ListIcon />
+            My Car&apos;s Booking
+          </NavLink>
+          <NavLink to="" className="flex gap-4 pb-2">
+            <CarPlusIcon />
+            Add New Car
+          </NavLink>
+        </div>
+        <NavLink to="" className="flex gap-4 py-2">
+          <LogoutIcon />
+          Logout
+        </NavLink>
+      </nav>
+    )
+  )
+}
