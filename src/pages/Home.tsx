413c003e1c10349aef346734f7c1a2e1ad10cbd4
Varma Cephas <varmac231@gmail.com>
2025-10-02 15:04:14 +0200

    feat: implement home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,12 @@ export default function Home() {
   //     setLoggedIn(prev=>!prev)
   //   }
   return (
-    <div className="font-lora mx-auto flex min-h-screen flex-col gap-8 py-20 text-center text-gray-100">
+    <div className="mx-auto flex min-h-screen flex-col gap-8 py-20 text-center font-lora text-gray-100">
       {/* the button below simulates logging the user in and out, it can be removed after logging in functionality is implemented */}
       {/* <button onClick={handleLogin}>{loggedIn? "Logout":"Login"}</button> */}
       <h1 className="pb-16 pt-12 text-5xl">
         <span className="block">MONI</span>
-        <span className="font-lora block italic">share</span>
+        <span className="block font-lora italic">share</span>
       </h1>
       <p className="py-12 pb-24 text-2xl">
         Start sharing your Monis <span className="block"> with the world</span>
