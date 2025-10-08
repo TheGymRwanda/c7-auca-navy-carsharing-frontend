ffb0d619b45ab7f2a2f8f5b44ba0245914b69b64
kezagiselle <kezagiselle776@gmail.com>
2025-10-08 08:26:21 +0200

    feat: changes on the AuthInputs

--- src/components/AuthInputs.tsx
+++ src/components/AuthInputs.tsx
@@ -6,11 +6,12 @@ interface AuthInputProps {
   value: string
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
   icon?: React.ReactNode
+  error?: string
 }
 
 function AuthInputs({ type = 'text', placeholder, value, onChange, icon }: AuthInputProps) {
   return (
-    <div className="flex items-center border rounded-full px-5 py-4 w-full bg-[#64A1C0]">
+    <div className="flex w-full items-center rounded-full border bg-[#64A1C0] px-5 py-4">
       {icon && <div className="mr-2 text-white">{icon}</div>}
 
       <input
@@ -18,7 +19,7 @@ function AuthInputs({ type = 'text', placeholder, value, onChange, icon }: AuthI
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="flex-1 outline-none bg-transparent text-black placeholder-white/80 font-semibold"
+        className="flex-1 bg-transparent font-semibold text-black outline-none placeholder:text-white/80"
       />
     </div>
   )
