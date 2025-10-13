4af93e5e5d322905893a0093b30a869cfa66270f
Gym Bigogwe <gymbigogwe@Bigogwes-iMac.local>
2025-10-13 17:41:10 +0200

    fix: i deleted a duplicated folder of utilies and created an env file for secure information

--- src/pages/CarsDetails.tsx
+++ src/pages/CarsDetails.tsx
@@ -1,5 +1,5 @@
 import { useParams, useNavigate } from 'react-router-dom'
-import { fetchWithAuth } from '../utils/api'
+import { fetchWithAuth } from '../util/auth'
 import { useEffect, useState } from 'react'
 import CarIcon from '../assets/CarIcon'
 import Attention from '../assets/Attention.png'
@@ -8,7 +8,7 @@ import HorseIcon from '../assets/HorseIcon'
 import { ChevronBackIcon } from '../assets/ChevronBackIcon'
 import FuelIcon from '../assets/FuelIcon'
 import ProfileIcon from '../assets/ProfileIcon'
-import { Car, CarType } from '../utils/types'
+import { Car, CarType } from '../types/CarTypes'
 
 export default function CarsDetails() {
   const { id } = useParams<{ id: string }>()
@@ -50,7 +50,7 @@ export default function CarsDetails() {
         <button onClick={() => navigate(-1)} className="mt-14">
           <ChevronBackIcon />
         </button>
-        <h1 className="mx-auto mt-14 font-lora text-2xl">DETAILS</h1>
+        <h1 className="font-lora mx-auto mt-14 text-2xl">DETAILS</h1>
       </div>
 
       <div className="mt-6">
@@ -94,7 +94,7 @@ export default function CarsDetails() {
         </div>
 
         {car.state && (
-          <div className="flex items-center gap-2 font-lora text-white">
+          <div className="font-lora flex items-center gap-2 text-white">
             <img
               src={Attention}
               alt="Attention"
