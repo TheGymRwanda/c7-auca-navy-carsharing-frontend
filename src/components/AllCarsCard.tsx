1151d54df19b2bb96e90b348c0a866e40d96139b
Mafupa <ryankarera@gmail.com>
2025-10-14 12:29:07 +0200

    feat: created custom hook to fetch all cars information + User and CarWithDetails interfaces

--- src/components/AllCarsCard.tsx
+++ src/components/AllCarsCard.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import ProfileIcon from '../assets/ProfileIcon'
 import CarIcon from '../assets/CarIcon'
 
-type Props = {
+type AllCarsCardProps = {
   id: string
   name: string
   imageUrl: string
@@ -10,24 +10,24 @@ type Props = {
   type: string
 }
 
-const AllCarsCard = ({ id, name, imageUrl, owner, type }: Props) => (
-  <div className="flex h-[220px] w-full rounded-[10px] bg-[#3e7591] px-4 py-2">
-    <img src={imageUrl} className="h-full w-auto" />
-    <div className="flex h-full flex-col justify-around">
-      <h1 className="text-start text-2xl">{name}</h1>
-      <div className="flex">
-        <ProfileIcon />
-        <h2>{owner}</h2>
+export default function AllCarsCard({ id, name, imageUrl, owner, type }: AllCarsCardProps) {
+  return (
+    <div className="flex h-[220px] w-full rounded-[10px] bg-[#3e7591] px-4 py-2">
+      <img src={imageUrl} className="h-full w-auto" />
+      <div className="flex h-full flex-col justify-around">
+        <h1 className="text-start text-2xl">{name}</h1>
+        <div className="flex">
+          <ProfileIcon />
+          <h2>{owner}</h2>
+        </div>
+        <div className="flex">
+          <CarIcon />
+          <h2>{type}</h2>
+        </div>
+        <Link to={`/cars/${id}`} className="font-bold text-[#f8fcad]">
+          Show details
+        </Link>
       </div>
-      <div className="flex">
-        <CarIcon />
-        <h2>{type}</h2>
-      </div>
-      <Link to={`/cars/${id}`} className="font-bold text-[#f8fcad]">
-        Show details
-      </Link>
     </div>
-  </div>
-)
-
-export default AllCarsCard
+  )
+}
--- src/pages/ShowAllCars.tsx
+++ src/pages/ShowAllCars.tsx
@@ -1,50 +1,14 @@
-import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import useAllCars from '../hooks/useAllCars'
 import { ChevronBackIcon } from '../assets/ChevronBackIcon'
-import { fetchWithAuth } from '../utils/api'
 import AllCarsCard from '../components/AllCarsCard'
-
-interface Car {
-  id: string
-  name: string
-  ownerId: string
-  model: string
-  licensePlate: string
-  horsepower: string
-  fuelType: string
-  state: string
-  carTypeId: string
-}
 
 export default function ShowAllCars() {
   const navigate = useNavigate()
-  const [carsList, setCarsList] = useState([])
-  const [loading, setLoading] = useState(true)
+  const { carsList, loading, error } = useAllCars()
 
-  useEffect(() => {
-    async function getAllCars() {
-      try {
-        const carsData = await fetchWithAuth(`/cars`)
-
-        const fullCarsData = await Promise.all(
-          carsData.map(async (car: Car) => {
-            const typeData = await fetchWithAuth(`/car-types/${car.carTypeId}`)
-            const ownerData = await fetchWithAuth(`/users/${car.ownerId}`)
-            return { ...car, typeData, ownerData }
-          }),
-        )
-
-        setCarsList(fullCarsData)
-      } catch (error) {
-        console.error('Failed to fetch cars:', error)
-      } finally {
-        setLoading(false)
-      }
-    }
-    getAllCars()
-  }, [])
-
-  if (loading) return <p className="mt-10 text-center text-white">Loading car details...</p>
+  if (loading) return <p className="mt-10 text-center">Loading car details...</p>
+  if (error) return <p className="mt-10 text-center">Error: {error.message}</p>
 
   return (
     <main className="mx-auto flex min-h-screen flex-col gap-8 py-20 text-center font-lora text-gray-100">
