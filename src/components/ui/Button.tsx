4bf33204f0a5b04f270fa6267e75c16146ca8874
Shema <shemaphilbert8@gmail.com>
2025-10-15 12:02:09 +0200

    chore: made button component responsive

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -12,7 +12,7 @@ const Button = ({
   className = '',
 }: ButtonProps) => {
   const baseClasses =
-    'w-4/5 mx-auto rounded-full border-2 py-2 text-lg font-semibold text-center transition-all duration-300'
+    'w-5/6 md:w-2/3 lg:w-1/2 mx-auto rounded-full border-2 py-2 text-lg font-semibold text-center transition-all duration-300'
 
   const variantClasses = clsx({
     'border-white bg-white text-cyan-800 hover:bg-gray-200 hover:border-gray-100':
